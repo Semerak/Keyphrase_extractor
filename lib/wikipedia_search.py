1049eb5b63562eb015d9f3f0f54591b46b16a531
Semerak <48312228+Semerak@users.noreply.github.com>
2021-01-31 22:25:25 +0200

    refactoring

--- lib/wikipedia_search.py
+++ lib/wikipedia_search.py
@@ -1,17 +1,25 @@
 import wikipedia
 
 
-def summary(word):
+def summary(word) -> str:
+    """Return information about word from wikipedia."""
     result = ""
+
     try:
         result = wikipedia.summary(word, chars=100, auto_suggest=False)
+
     except wikipedia.exceptions.DisambiguationError:
         result = "Disambiguation: " + " ,".join(wikipedia.search(word, results=4))
+
     except:
         result = "No page"
+
     return result
 
 
-def list_summary(table):
-    return [{'word': line['word'], 'wiki': summary(line['word'])} for line in table]
+def list_summary(table) -> list:
+    """Add to table of words information from wikipedia."""
+    for line in table:
+        line['wiki'] = summary(line['word'])
+    return table
 
